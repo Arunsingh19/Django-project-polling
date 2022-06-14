1d61df3134133c0fd5495f606537d82404c90746
Arunsingh <arunsingh19101999@gmail.com>
2022-06-14 12:11:03 +0530

    django_Settings_module

--- zappit/settings.py
+++ zappit/settings.py
@@ -10,23 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path 
-import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-q^!iub^7m@a^w$+g2%5o!!o)sc3s=plu6j(4gl@6%a0@izc7tu'
+SECRET_KEY = 'django-insecure-q^!iub^7m@a^w$+g2%5o!!o)sc3s=plu6j(4gl@6%a0@izc7tu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
-# ALLOWED_HOSTS = ['voting-arunsingh.herokuapp.com']
+ALLOWED_HOSTS = ['voting-arunsingh.herokuapp.com']
 
 
 # Application definition
@@ -40,13 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts',
     'rest_framework',
-    # 'debug_toolbar'
 ]
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -79,16 +75,16 @@ WSGI_APPLICATION = 'zappit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'vote',
-#         'HOST': 'localhost',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Arun@7708',
-#         'PORT' : '5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vote',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': 'Arun@7708',
+        'PORT' : '5432'
+    }
+}
 
 
 # Password validation
@@ -126,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
